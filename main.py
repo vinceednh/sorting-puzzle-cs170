1e479b7ffae745dcdfcf0822963eb1456d116fe2
Vincent Dinh <vincent.dinh401@gmail.com>
2026-02-11 19:59:12 -0800

    Created barebones main.py to test ucs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-
+from puzzle import PuzzleState
+from search import uniform_cost_search
 
 # This makes the size of the board so it can be run with multiple different sizes
 def make_goal(size):
@@ -16,6 +17,28 @@ def make_goal(size):
     return goal
 
 def main():
+    size = int(input("Enter puzzle size: "))
+
+    print("Enter board rows(use 0 for blank tile):")
+    board = []
+
+    for i in range(size):
+        row = list(map(int, input().split()))
+        board.append(row)
+
+    goal = make_goal(size)
+    start_state = PuzzleState(board, size)
+
+    # Uniform Cost Search
+    result, nodes_expanded, max_queue_size = uniform_cost_search(start_state, goal)
+
+    if result is None:
+        print("No solution found.")
+    else:
+        print("Goal reached!")
+        print(f"Solution depth was {result.cost}")
+        print(f"Nodes expanded: {nodes_expanded}")
+        print(f"Max queue size: {max_queue_size}")
 
 if __name__ == "__main__":
     main()
